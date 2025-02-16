cfde2e256b028334db2d3a7b5b4674d897bdf4e2
Avaer Kazmer <ultimus@gmail.com>
2025-02-16 15:22:50 -0800

    Bugfix context typing

--- u8.ts
+++ u8.ts
@@ -85,7 +85,7 @@ const ADDENDUM_SERIALIZERS = (() => {
       const canvas = document.createElement('canvas');
       canvas.width = image.width;
       canvas.height = image.height;
-      const context = canvas.getContext('2d');
+      const context = canvas.getContext('2d')!;
       context.drawImage(image, 0, 0);
       image = context.getImageData(0, 0, image.width, image.height);
     }
